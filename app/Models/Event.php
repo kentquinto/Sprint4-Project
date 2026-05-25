2104820366e76c156c1294750f7c7e4acb96426d
Kent Quinto <kentquinto1@gmail.com>
2026-05-25 21:24:31 +0200

    removed manual carbon::parse and created casts to event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,16 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     protected $fillable = [
-    'title',
-    'description',
-    'location',
-    'entry_fee',
-    'max_players',
-    'date_time',
-    'status',
-    'creator_id',
-    'game_id'
+        'title',
+        'description',
+        'location',
+        'entry_fee',
+        'max_players',
+        'date_time',
+        'status',
+        'creator_id',
+        'game_id',
     ];
+
+    protected function casts(): array
+    {
+        return [
+            'date_time'  => 'datetime',
+            'entry_fee'  => 'decimal:2',
+        ];
+    }
 
     public function creator () {
         return $this->belongsTo(User::class, 'creator_id');
